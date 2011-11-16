f8008c232b937e9536ec2b91fe91a5d65f2b76c1
isaacs <i@izs.me>
2011-11-16 12:07:21 -0800

    the program

---
+++ nosync.js
@@ -0,0 +1,22 @@
+var fs = require("fs")
+  , child_process = require("child_process")
+
+process.nextTick(function () {
+  Object.keys(process.binding("natives")).map(function (n) {
+    if (n === "sys") return []
+    var module = require(n)
+    return Object.keys(module).filter(function (f) {
+      return f.match(/Sync$/)
+    }).map(function (f) {
+      return [module, f, n]
+    })
+  }).reduce(function (l, r) {
+    return l.concat(r)
+  }, []).forEach(function (fn) {
+    fn[0][fn[1]] = thrower(fn)
+  })
+})
+
+function thrower (fn) { return function () {
+  throw new Error("Sync function called after first tick: "+fn[2]+"."+fn[1])
+}}
